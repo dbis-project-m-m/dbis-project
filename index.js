47ad3c139c1c92fd8cb5b262383756f319ce6fbd
Mihir <mihirkaushik03@gmail.com>
2022-11-06 11:55:46 +0530

    sign_in final

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express= require("express")
-const mysql =require("mysql")
-const path=require("path")
+const express = require("express")
+const mysql = require("mysql")
+const path = require("path")
 const cookieParser = require("cookie-parser");
 const sessions = require('express-session');
 
@@ -39,7 +39,7 @@ app.use(cookieParser());
 // a variable to save a session
 var session;
 
-
+// console.log(req.session.email)
 
 db.connect((err) => {
     if (err) {
@@ -52,7 +52,18 @@ app.get('/', (req, res) => {
     res.render('home.ejs');
 })
 
+app.get('/create_acct', (req, res)=>{
+    console.log("i am here")
+    var username=req.query.username
+    var email=req.query.email
+    var password=req.query.password
 
+    db.query("insert into login(email, username, password) values (?, ?, ?)", [ email, username, password], (err, result)=>{
+        if(err) console.log(err)
+
+        res.redirect('/sign_in')
+    })
+})
 app.get('/search', (req, res) => {
     var email = req.query.email
     var password = req.query.password
@@ -73,9 +84,9 @@ app.get('/search', (req, res) => {
             // prompt("useremail does not exist")
         }
         else if (result[0].password == password) {
-            session=req.session;
-        session.email=req.query.email;
-        console.log(req.session)
+            session = req.session;
+            session.email = req.query.email;
+            console.log(req.session)
             res.render('home.ejs', { result })
         }
         else {
@@ -88,13 +99,21 @@ app.get('/search', (req, res) => {
 
 })
 
-app.get('/logout',(req,res) => {
+app.get('/logout', (req, res) => {
     req.session.destroy();
-    res.redirect('/');
+    // console.log(req.session.user)
+    res.redirect('/sign_in');
 });
 
 app.get('/home', (req, res) => {
-    res.render('home.ejs');
+    if(req.session.email!=undefined)
+    {
+        let result6={email: req.session.email}
+        res.render('home.ejs', {email: req.session.email});
+    }
+    else {
+        res.render('home.ejs', {result6:[]})
+    }
 })
 
 app.get('/sign_in', (req, res) => {
@@ -107,28 +126,44 @@ app.get('/amenities', (req, res) => {
     res.render('amenities.ejs')
 })
 app.get('/find_flight', (req, res) => {
+    if (req.session.email != undefined) {
+        console.log(req.session.email)
+    }
+    else if (req.session.email == undefined) {
+        console.log("req.session.email")
+    }
     // console.log(session.email)
     res.render('find_flight.ejs', { results: [] })
 })
-app.get('/booking/:flight_id', (req, res) => {
+app.get('/booking/:flight_id/:arrival_schedule_id', (req, res) => {
 
-    let { flight_id } = req.params;
-    console.log(flight_id)
-
-
-
-    res.render('booking.ejs')
+    if(req.session.email!=undefined)
+    {
+        let result = {
+            flight_id: req.params.flight_id,
+            schedule_id: req.params.arrival_schedule_id
+        }
+        console.log(result.schedule_id)
+        res.render('booking.ejs', { flight_id: req.params.flight_id,
+            schedule_id: req.params.arrival_schedule_id, abc: 3 })
+    }
+    else{
+        res.render('sign_in.ejs')
+    }
 })
 
-app.get('/bookticket', (req, res) => {
+app.get('/bookticket/:flight_id/:schedule_id', (req, res) => {
     let data = {
         name: req.query.name,
         email: req.query.email,
         mobile_no: req.query.mobile_no,
-        aadhar_no: req.query.aadhar_no
+        aadhar_no: req.query.aadhar_no,
+        class: req.query.class
     }
-
+    console.log(req.params.flight_id)
     let sql2 = `select aadhar_no from passengers`
+    let sql3 = `select * from bookings where flight_id=${req.params.flight_id} and schedule_id=${req.params.schedule_id}`
+
     db.query(sql2, (err, result) => {
         if (err) { console.log(err) }
         else {
@@ -141,10 +176,23 @@ app.get('/bookticket', (req, res) => {
             }
             if (!check && data.name != null && data.email != null && data.mobile_no != null && data.aadhar_no != null) {
                 let sql1 = `insert into passengers(name, email, mobile_no, aadhar_no) values ('${data.name}','${data.email}','${data.mobile_no}','${data.aadhar_no}')  `
-                db.query(sql1, (err, result) => {
+                let sql4 = `select passenger_id from passengers where aadhar_no='${data.aadhar_no}'`
+                db.query(sql1, (err, result1) => {
                     if (err) console.log(err)
-                    console.log(data.name)
-                    res.render('booking.ejs')
+
+                    db.query(sql3, (err, result2) => {
+                        if (err) console.log(err)
+                        db.query(sql4, (err, result3) => {
+                            if (err) console.log(err)
+                            console.log("lodu")
+                            console.log(result2[0].booking_id)
+                            db.query("insert into tickets(booking_id, passenger_id) values (?, ?)", [result2[0].booking_id, result3[0].passenger_id], (err, result4) => {
+                                console.log(data.name)
+                                res.render('booking.ejs')
+                            })
+                        })
+                    })
+
                 })
             }
             else {
@@ -165,7 +213,7 @@ app.get('/output', (req, res) => {
         date: req.query.date
     }
 
-    let sql = `select f.flight_id, f.airline, r.from_route, r.to_route, af.arrival_time, af.duration 
+    let sql = `select f.flight_id, f.airline, r.from_route, r.to_route, af.arrival_time, af.duration, af.arrival_schedule_id 
     from flight as f join arrival_flights as af on f.flight_id=af.flight_id join route as r on r.route_id=af.route_id
     where r.from_route='${input.from}' and r.to_route='${input.to}' and date(af.arrival_time)='${input.date}'`
     db.query(sql, (err, results) => {
